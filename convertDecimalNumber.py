c129d223b60936178624bb74195d6ca803ba3c08
Niv Ben Abat <niv.benavat@gmail.com>
2019-01-10 18:04:57 +0200

    Add files via upload

---
+++ convertDecimalNumber.py
@@ -0,0 +1,14 @@
+# Python program to convert decimal number
+# into binary, octal and hexadecimal number system
+
+
+dec = int(input("Enter any Number: "))
+print("The decimal value of", dec, "is: ")
+print(bin(dec),"in binary."))
+print(oct(dec),"in octal."))
+print(hex(dec),"in hexadecimal."))
+
+
+
+# ======= Output ==========
+
